34af0a1458fd813a664339ca4e051f4f69f0dddf
jianqianyan <jianqianyan@qq.com>
2025-04-11 16:40:00 +0800

    feat: 去除ai生成的分号

--- aiServices/index.ts
+++ aiServices/index.ts
@@ -1,26 +1,26 @@
 import { Siliconflow } from "./manufacturer/siliconflow";
 
 export interface Manufacturer {
-  modelName: string;
-  onmessage: (str: string) => void;
-  onend: () => void;
+  modelName: string
+  onmessage: (str: string) => void
+  onend: () => void
 }
 
-type ManufacturerConstructor = new (...args: any[]) => Manufacturer;
+type ManufacturerConstructor = new (...args: any[]) => Manufacturer
 
 const list: Record<string, ManufacturerConstructor> = {
   siliconflow: Siliconflow
 };
 
 export class AiService {
-  server: Manufacturer;
+  server: Manufacturer
 
   constructor(name: string, options: string[]) {
-    const Constructor = list[name];
+    const Constructor = list[name]
     if (Constructor) {
-      this.server = new Constructor(...options);
+      this.server = new Constructor(...options)
     } else {
-      throw new Error(`Unknown manufacturer: ${name}`);
+      throw new Error(`Unknown manufacturer: ${name}`)
     }
   }
 }
